38f3ab5561f14df560c69bade3589e921319df94
Fatma-Alwasmi <hotbreakfast01@gmail.com>
2025-02-02 23:01:12 -0500

    Add files via upload

---
+++ PatchFinder.py
@@ -0,0 +1,57 @@
+
+
+#take txt file of cves 
+#look for input keyword
+#look for all corresponding hash with that keyword
+#save them in keyword.txt
+#loop through every hash in keyword.txt
+    #for each hash:
+        #if |deletions| + insertions <= 3:
+            #add the cve:hash:description in keywordFinal.txt
+
+import urllib
+import requests
+import re
+
+################ find cve related to keyword BEGIN ###################
+keyword = input("type a keyword ")
+with open("6.7_security.txt", "r") as cve_file:
+    for line in cve_file:
+        if "CVEs fixed in" in line:
+            version = line.strip()
+        elif keyword in line:
+            with open("cve_temp.txt", "a") as the_file:
+                format_line = f"{version}{line}"
+                the_file.write(format_line)
+################ find cve related to keyword END ###################  
+
+hash_pattern = r'CVE-\d{4}-\d{4,5}:\s+([a-f0-9]{40})'
+linux_commit = "https://github.com/torvalds/linux/commit/"
+
+with open("cve_temp.txt", "r") as the_file:
+    for line in the_file:
+        match = re.search(hash_pattern, line)
+        if match:
+            hash = ''.join(match.group(1)) 
+            url = linux_commit+hash
+            response = requests.get(url)
+            content = response.text
+            #check if patch is <= 3 lines of code
+            if " 1 change:" in content:
+                with open("cve_final.txt", "a") as the_file1:
+                    print("1 change")
+                    the_file1.write(line)
+
+            elif " 2 changes:" in content:
+                with open("cve_final.txt", "a") as the_file1:
+                    print("2 changes")
+                    the_file1.write(line)
+
+            elif " 3 changes:" in content:
+                with open("cve_final.txt", "a") as the_file1:
+                    print("3 changes")
+                    the_file1.write(line)
+            
+
+
+
