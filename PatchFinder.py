ca03f8b09f13a134b094fe58e3151517da71fa35
Fatma-Alwasmi <hotbreakfast01@gmail.com>
2025-02-04 11:32:24 -0500

    Update PatchFinder.py

--- PatchFinder.py
+++ PatchFinder.py
@@ -13,7 +13,6 @@ import re
 import json
 import os
 
-################ find cve related to keyword BEGIN ###################
 keyword = input("BUG: ")
 
 if " " in keyword:
@@ -24,7 +23,7 @@ else:
 input_file = "kernel_cves.json"
 final_file = final_file_name + ".txt"   
 linux_commit = "https://github.com/torvalds/linux/commit/"
-
+count = 0
 
 with open(input_file, "r") as cve_file:
     data = json.load(cve_file)
@@ -36,24 +35,29 @@ for cve in data.keys():
             description = data[cve]['nvd_text'] 
             if keyword in description:
                 url = linux_commit+hash
-                response = requests.get(url)
-                content = response.text
-                #check if patch is <= 3 lines of code
-                if " 1 change:" in content:
-                    format_line = f"{cve}:{url}\n"
-                    with open(final_file, "a") as the_file:
-                        print("1 change")
-                        the_file.write(format_line)
+                if requests.get(url).status_code == 200:
+                    response = requests.get(url)
+                    content = response.text
+                    #check if patch is <= 3 lines of code
+                    if " 1 change:" in content:
+                        format_line = f"{cve}:{url}\n"
+                        with open(final_file, "a") as the_file:
+                            print(format_line)
+                            the_file.write(format_line)
+                            count+=1
 
-                elif " 2 changes:" in content:
-                    format_line = f"{cve}:{url}\n"
-                    with open(final_file, "a") as the_file:
-                        print("2 changes")
-                        the_file.write(format_line)
+                    elif " 2 changes:" in content:
+                        format_line = f"{cve}:{url}\n"
+                        with open(final_file, "a") as the_file:
+                            print(format_line)
+                            the_file.write(format_line)
+                            count+=1
 
-                elif " 3 changes:" in content:
-                    format_line = f"{cve}:{url}\n"
-                    with open(final_file, "a") as the_file:
-                        print("3 changes")
-                        the_file.write(format_line)
-            
+                    elif " 3 changes:" in content:
+                        format_line = f"{cve}:{url}\n"
+                        with open(final_file, "a") as the_file:
+                            print(format_line)
+                            the_file.write(format_line)
+                            count+=1
+
+print(f"Found {count} {keyword} bug patches")
