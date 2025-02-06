bd2ad4b3cbc1c8e25c61b1232cac3344accc0332
Fatma-Alwasmi <hotbreakfast01@gmail.com>
2025-02-06 14:57:06 -0500

    Update PatchFinder.py

--- PatchFinder.py
+++ PatchFinder.py
@@ -11,7 +11,6 @@ import urllib
 import requests
 import re
 import json
-import os
 
 keyword = input("BUG: ")
 
@@ -33,27 +32,14 @@ for cve in data.keys():
         if data[cve]['fixes'] != "" and data[cve]['nvd_text'] != "":
             hash = data[cve]['fixes']
             description = data[cve]['nvd_text'] 
-            if keyword in description:
+            if keyword.lower() in description.lower():
                 url = linux_commit+hash
                 if requests.get(url).status_code == 200:
                     response = requests.get(url)
                     content = response.text
                     #check if patch is <= 3 lines of code
-                    if " 1 change:" in content:
-                        format_line = f"{cve}:{url}\n"
-                        with open(final_file, "a") as the_file:
-                            print(format_line)
-                            the_file.write(format_line)
-                            count+=1
-
-                    elif " 2 changes:" in content:
-                        format_line = f"{cve}:{url}\n"
-                        with open(final_file, "a") as the_file:
-                            print(format_line)
-                            the_file.write(format_line)
-                            count+=1
-
-                    elif " 3 changes:" in content:
+                    #modify to check if one file changed only '>1 changed file'
+                    if (" 1 change:" in content or " 2 changes:" in content or " 3 changes:" in content) and ">1 changed file" in content:
                         format_line = f"{cve}:{url}\n"
                         with open(final_file, "a") as the_file:
                             print(format_line)
