56d08b3ffbd5d4d652ff59cbf43690280c850b5d
Fatma <hotbreakfast01@gmail.com>
2025-04-16 10:46:43 -0400

    Update PatchFinder.py

--- PatchFinder.py
+++ PatchFinder.py
@@ -1,4 +1,4 @@
-#take txt file of cves
+#take txt file of cves 
 #look for input keyword
 #look for all corresponding hash with that keyword
 #save them in keyword.txt
@@ -14,7 +14,8 @@ import json
 import subprocess
 import os
 
-os.chdir("/home/falwasmi/linux-kernel")
+linux_dir = input("linux dir? ")
+os.chdir(linux_dir)
 
 keyword = input("BUG: ")
 
@@ -24,7 +25,7 @@ else:
     final_file_name = keyword
 
 input_file = "/home/falwasmi/kernel_cves.json"
-final_file = "/home/falwasmi/" + final_file_name + ".txt"
+final_file = "/home/falwasmi/" + final_file_name + ".txt"   
 linux_commit = "https://github.com/gregkh/linux/commit/"
 count = 0
 
@@ -35,7 +36,7 @@ for cve in data.keys():
     if 'fixes' in data[cve] and 'nvd_text' in data[cve] and 'cmt_msg' in data[cve]:
         if data[cve]['fixes'] != "" and data[cve]['nvd_text'] != "":
             hash = data[cve]['fixes']
-            description = data[cve]['nvd_text']
+            description = data[cve]['nvd_text'] 
             description2 = data[cve]['cmt_msg']
             if keyword.lower() in description.lower() or keyword.lower() in description2.lower():
                 try:
@@ -51,22 +52,22 @@ for cve in data.keys():
 
                 output_lines = result.stdout.strip().splitlines()
                 last_two_lines = output_lines[-2:] if len(output_lines) >= 2 else output_lines
-
-
+        
+                        
                 line1 = last_two_lines[0].strip()  # ex "fs/jfs/jfs_dmap.c | 3 ++-"
                 line2 = last_two_lines[1].strip()  # ex "1 file changed, 2 insertions(+), 1 deletion(-)"
-
+        
                 three_or_less = False
                 if " 1 " in line1 or " 2 " in line1 or " 3 " in line1:
                     three_or_less = True
-
+        
                 one_file_changed = False
                 if "1 file changed" in line2:
                     one_file_changed = True
 
 
                 if three_or_less and one_file_changed:
-                    url = linux_commit+hash
+                    url = linux_commit+hash 
                     format_line = f"{cve}  {url}.diff\n"
                     with open(final_file, "a") as the_file:
                         print(format_line)
